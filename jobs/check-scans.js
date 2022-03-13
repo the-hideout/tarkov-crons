973c78cd333c54c332ff897879765b8b72983df6
Razzmatazz <razz@badkarma.xyz>
2022-03-13 08:19:11 -0500

    fix references to db module

--- jobs/check-scans.js
+++ jobs/check-scans.js
@@ -1,4 +1,4 @@
-const connection = require('../modules/db-connection');
+const { connection } = require('../modules/db-connection');
 const webhook = require('../modules/webhook');
 
 const ignoreSources = [
